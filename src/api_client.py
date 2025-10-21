35e34212f3acef2c224758542828d37b9d999a3a
Jessica Coffman <Jessicac667@outlook.com>
2025-10-21 10:46:02 -0400

    testing

--- src/api_client.py
+++ src/api_client.py
@@ -16,3 +16,4 @@ response = requests.get(url, headers=headers)
 
 print(response.status_code)
 print(response.json()[0])  # testing to see if it works. should pull first record
+#testing to see if it works now
