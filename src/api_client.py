b948bad3e237dc3c2f486dc1c067c704b8727734
Jessica-coffman <jessicac667@outlook.com>
2025-10-21 12:33:38 -0400

    Update api_client.py

--- src/api_client.py
+++ src/api_client.py
@@ -1,12 +1,16 @@
 import requests
 import os
+import json
+import csv
+import pandas as pd
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv()
-
+year = input("What year?:    ")
 api_key = os.getenv("RANSOMWARE_API_KEY")
 
-url = "https://api-pro.ransomware.live/victims/"
+url = "https://api-pro.ransomware.live/victims/?year=" + str(year)
 headers = {
     "accept": "application/json",
     "X-API-KEY" : api_key
@@ -14,5 +18,11 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
-print(response.status_code)
-print(response.json()[0])
+
+data = response.json() # extracting json content
+
+records = data['victims'] # gets it to where we want
+df = pd.json_normalize(records) # takes the victim records, flattens them, puts them into Data Frame
+df_with_filters = df[['country', 'activity', 'attackdate', 'victim', 'group']]
+
+df_with_filters.to_csv("ransowmare_victims-" + date.today().strftime("%Y-%m-%d") + '.csv', index=False)
