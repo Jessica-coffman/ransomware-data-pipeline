9ad55f57b4f6f7b3f38854f51dce4c15238678f6
Jessica Coffman <Jessicac667@outlook.com>
2025-10-21 12:54:46 -0400

    fixed ransomware spelling

--- src/api_client.py
+++ src/api_client.py
@@ -25,5 +25,4 @@ records = data['victims'] # gets it to where we want
 df = pd.json_normalize(records) # takes the victim records, flattens them, puts them into Data Frame
 df_with_filters = df[['country', 'activity', 'attackdate', 'victim', 'group']]
 
-df_with_filters.to_csv("ransowmare_victims-" + date.today().strftime("%Y-%m-%d") + '.csv', index=False)
-#test
+df_with_filters.to_csv("ransomware_victims-" + date.today().strftime("%Y-%m-%d") + '.csv', index=False)
