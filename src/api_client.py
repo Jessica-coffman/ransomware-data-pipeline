5d97a9c55f8a64bdac399272a9357bfaf069bc0d
Jessica Coffman <Jessicac667@outlook.com>
2025-10-21 14:23:25 -0400

    updated BQ

--- src/api_client.py
+++ src/api_client.py
@@ -7,10 +7,16 @@ from dotenv import load_dotenv
 from datetime import date
 
 load_dotenv()
-year = input("What year?:    ") #asks user input for year 
+year = input("What year?:    ")
 api_key = os.getenv("RANSOMWARE_API_KEY")
+project_id = os.getenv("PROJECT_ID")
+table_name = os.getenv("TABLE_NAME")
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_key
 todays_year = int(date.today().strftime('%Y'))
-if int(year) >= 2022 and int(year) <= todays_year: #if the year is not between 2022 and this year, returns error
+
+if int(year) >= 2022 and int(year) <= todays_year:
+
     url = "https://api-pro.ransomware.live/victims/?year=" + str(year)
     headers = {
         "accept": "application/json",
@@ -18,15 +24,21 @@ if int(year) >= 2022 and int(year) <= todays_year: #if the year is not between 2
     }
 
     response = requests.get(url, headers=headers)
-
-
     data = response.json() # extracting json content
-
     records = data['victims'] # gets it to where we want
     df = pd.json_normalize(records) # takes the victim records, flattens them, puts them into Data Frame
     df_with_filters = df[['country', 'activity', 'attackdate', 'victim', 'group']]
 
-    df_with_filters.to_csv("ransomware_victims_" + year + '.csv', index=False)
-else:
-    print("Error: year out of bounds ")
+    df_with_filters.to_csv("ransomware_victims_" + year + '.csv', index=False) # saves CSV
 
+    #import into GCP 
+    try:
+        table_id = project_id + '.' + table_name + '.ransomware_victims_' + year
+        client = bigquery.Client()
+        upload = client.load_table_from_dataframe(df_with_filters, table_id)
+        upload.result()
+        print(f"uploaded {len(df_with_filters)} rows to {table_id}")
+    except Exception as e:
+        print("Error: ", e)
+else:
+    print('Error: year out of bounds')
